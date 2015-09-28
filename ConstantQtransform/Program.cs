290e002608c4e83e767817f8f9fac25d938003a0
yusukeinyos <aero.n.404.smith@gmail.com>
2015-09-29 03:31:15 +0900

    暫定

--- ConstantQtransform/Program.cs
+++ ConstantQtransform/Program.cs
@@ -12,26 +12,31 @@ namespace ConstantQtransform
 {
     class Program
     {
-        public static double fs = 22050;
+        public static double fs;
         public static double fmin = 60;
         public static double fmax = 6000;
         public static double fratio = 1.0 / 24; //1オクターブの周波数bin数
         public static double q_rate = 20.0 * fratio; //Q比
-        public static double Q = (int)(1.0/(Math.Pow(2,fratio)-1)*q_rate);
+        public static double Q = (int)(1.0 / (Math.Pow(2, fratio) - 1) * q_rate);
 
         static void Main(string[] args)
         {
-            double[] freqs = get_freqs(fmin, get_freq_num(fmin, fmax, fratio), fratio);
-            Complex[,] kernel = calcKernel_matrix(freqs);
-            double[,] kernel_pow = new double[kernel.GetLength(0), kernel.GetLength(1)];
-            for (int i = 0; i < kernel.GetLength(0); i++)
-            {
-                for (int j = 0; j < kernel.GetLength(1); j++)
-                {
-                    kernel_pow[i, j] = kernel[i, j].Magnitude;
-                }
-            }
-            CsvFileIO.CsvFileIO.WriteData("kernel.csv", kernel_pow);
+            //double[] freqs = get_freqs(fmin, get_freq_num(fmin, fmax, fratio), fratio);
+            //Complex[,] kernel = calcKernel_matrix(freqs);
+            //double[,] kernel_pow = new double[kernel.GetLength(0), kernel.GetLength(1)];
+            //for (int i = 0; i < kernel.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < kernel.GetLength(1); j++)
+            //    {
+            //        kernel_pow[i, j] = kernel[i, j].Magnitude;
+            //    }
+            //}
+            Tuple<double[][], int> tu = WaveFile.Load(@"C:\Users\優\Desktop\音素材\mix3.wav");
+            double[] wavdata = tu.Item1[0];
+            fs = tu.Item2;
+            double[,] output = constantQ_transformP(wavdata);
+
+            CsvFileIO.CsvFileIO.WriteData("output.csv", output);
         }
         //--------------------------------------------------------------------------
         //対数周波数bin数の計算
@@ -61,7 +66,7 @@ namespace ConstantQtransform
             for (int k = 0; k < freqs.Length; k++)
             {
                 double freq = freqs[k];
-                int N_k = (int)(fs * Q / freq);     //窓幅
+                int N_k = (int)((double)(fs * Q) / freq);     //窓幅
                 int start_win = (fftlen - N_k) / 2; //FFT窓の中心を解析部分に合わせる
                 double[] hamming = new double[N_k];
                 for (int i = 0; i < N_k; i++)
@@ -76,16 +81,80 @@ namespace ConstantQtransform
                 for (int i = 0; i < tmp_kernel.Length; i++)
                     d[i] = tmp_kernel[i].Magnitude;
                 double max = d.Max();
-                    for (int i = 0; i < fftlen; i++)
-                    {
-                        //if (tmp_kernel[i].Magnitude <= 0.0054)
-                        //    kernel[k, i] = Complex.Zero;
-                        //else
-                        kernel[k, i] = Complex.Conjugate(tmp_kernel[i]);
-                    }
+                for (int i = 0; i < fftlen; i++)
+                {
+                    //if (tmp_kernel[i].Magnitude <= 0.0054)
+                    //    kernel[k, i] = Complex.Zero;
+                    //else
+                    kernel[k, i] = Complex.Conjugate(tmp_kernel[i]);
+                }
             }
 
             return kernel;
         }
+        //--------------------------------------------------------------------------
+        //constant Q transform
+        static Complex[,] constantQ_transform(double[] wavdata)
+        {
+            double[] new_wavdata;
+            Complex[,] kernel;
+            Complex[,] output;
+            int wav_length = wavdata.Length;
+            int T_step;
+            int F_step;
+            int fft_step;
+            int fft_start_point;
+            int hop;
+
+            double[] freqs = get_freqs(fmin, get_freq_num(fmin, fmax, fratio), fratio);
+            kernel = calcKernel_matrix(freqs);
+
+            hop = (int)(Math.Round(0.01 * fs)); //100 frames per 1 sec
+            T_step = wav_length / hop;
+            F_step = freqs.Length;
+            fft_step = (int)(Math.Pow(2, Math.Ceiling((Math.Log(fs * Q / freqs[0], 2))))); //greater than Max of N[k]
+            new_wavdata = new double[wav_length + fft_step];
+            output = new Complex[F_step, T_step];
+
+            for (int i = 0; i < wav_length; i++)
+                new_wavdata[i + fft_step / 2] = wavdata[i];
+
+            for (int t = 0; t < T_step; t++)
+            {
+                fft_start_point = hop * t;
+                Complex[] partial_data = new Complex[fft_step];
+                for (int i = 0; i < fft_step; i++)
+                    partial_data[i] = new Complex(new_wavdata[fft_start_point + i], 0);
+
+                Complex[] fft = Nm.FastFourierTransform(partial_data, true);
+                Complex[] cq = new Complex[F_step];
+                for (int i = 0; i < F_step; i++)
+                    for (int j = 0; j < fft.Length; j++)         //fft.Length = fft_step
+                        cq[i] += fft[j] * kernel[i, j];
+
+                for (int f = 0; f < F_step; f++)
+                    output[f, t] = cq[f];
+            }
+            return output;
+        }
+        //--------------------------------------------------------------------------
+        //constant Q transform(power)
+        static double[,] constantQ_transformP(double[] wavdata)
+        {
+            double[,] output;
+            Complex[,] c = constantQ_transform(wavdata);
+
+            int I = c.GetLength(0), J = c.GetLength(1);
+            output = new double[I, J];
+            for (int i = 0; i < I; i++)
+            {
+                for (int j = 0; j < J; j++)
+                {
+                    output[i, j] = c[i, j].Magnitude * c[i, j].Magnitude;
+                }
+            }
+            return output;
+        }
+
     }
 }
